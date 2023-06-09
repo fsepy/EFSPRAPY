25228e23e5a4a9495927b541fd500fbe17081549
fuyans <fuyans@gmail.com>
2023-06-09 12:24:30 +0100

    few changes mcs1

--- test/test_mcs1.py
+++ test/test_mcs1.py
@@ -40,15 +40,17 @@ def test_mcs0_deterministic():
         lining_c=1000,
         lining_k=1.13,
 
-        receiver_ignition_temperature=-(300 + 273.15),
+        receiver_ignition_temperature=300 + 273.15,
         receiver_separation=2,
 
         ftp_chf=12.6e3,
         ftp_index=2,
         ftp_target=34592,
     )
-    # print(phi_1, phi_2, ftp, t_ig_ftp, t_ig_safir, t_max_safir, T_max_safir, fire_mode)
-    # assert abs(phi_1 - 0.291670) <= 1e-2, f'{phi_1}!=0.291670'
+    res_ = [510.4296006211892, 1.0, 1.1550856590671998, 0.3899564855008652, 0]
+    assert len(res) == len(res_), 'mismatch length'
+    for i in range(len(res)):
+        assert abs(res[0] - res_[0]) <= 1e-2, f'{res[0]}!={res_[0]}'
     # assert abs(phi_2 - 0.443859) <= 1e-2, f'{phi_2}!=0.443859'
     # assert abs(ftp - 56531.682) <= 1e-2, f'{ftp}!=56531.682'
     # assert abs(t_ig_ftp - 2235.) <= 1e-2, f'{t_ig_ftp}!=2250.'
@@ -56,12 +58,12 @@ def test_mcs0_deterministic():
     print(res)
 
 
-def test_mcs0_research_v2():
+def test_mcs0():
     input_kwargs = dict(
         CASE_1=dict(
-            n_simulations=100,
-            t_end=180. * 60.,
-            t_step=5.,
+            n_simulations=1000,
+            t_end=120. * 60.,
+            t_step=10.,
 
             opening_width=12,
             opening_height=3,
@@ -72,7 +74,7 @@ def test_mcs0_research_v2():
 
             fire_mode=dict(dist='discrete_', values='0,1,2', weights='0.07,0.59,0.34'),
             fire_fuel_density_MJm2=dict(dist="gumbel_r_", lbound=10, ubound=1200, mean=420, sd=126),  # 420
-            fire_hrr_density_kWm2=250,
+            fire_hrr_density_kWm2=dict(dist='uniform_', lbound=250, ubound=350),
             fire_growth_factor=0.0117,
             fire_t_lim=20. * 60.,
             fire_din_growth_factor=300,  # todo
@@ -87,11 +89,10 @@ def test_mcs0_research_v2():
             lining_rho=2000,
             lining_c=1000,
             lining_k=1.13,
-            lining_thermal_effusivity=1500,
 
-            receiver_ignition_temperature=300 + 273.15,
+            receiver_ignition_temperature=-300 + 273.15,
             receiver_emissivity=1,
-            receiver_separation=2,
+            receiver_separation=4,
 
             ftp_chf=12.6e3,
             ftp_index=dict(dist='uniform_', lbound=1, ubound=2),
@@ -105,10 +106,11 @@ def test_mcs0_research_v2():
     pbar = tqdm(total=sum([v['n_simulations'] for k, v in input_kwargs.items()]))
     mcs = MCS1()
     mcs.set_inputs_dict(input_kwargs)
-    mcs.run(n_proc=2, set_progress=lambda x: pbar.update(1), save=True, save_archive=False)
+    mcs.run(n_proc=61, set_progress=lambda x: pbar.update(1), save=True, save_archive=False)
+    pbar.close()
 
 
 if __name__ == "__main__":
     # test_calculate_incident_heat_flux_from_controlled_fire_cfast()
-    test_mcs0_research_v2()
     # test_mcs0_deterministic()
+    test_mcs0()
